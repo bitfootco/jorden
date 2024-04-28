a029c4a3e967b8424d05516a8638019ac3b4c877
Luke Patrick <luke@routerchowder.com>
2024-04-28 10:08:01 -0500

    feat: integrate Vite plugin from lib with the default Vite configuration file

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import generateRoutes from "./lib/routes.plugin";
 
 export default defineConfig({
   root: "src",
@@ -14,5 +15,6 @@ export default defineConfig({
     react({
       include: "**/*.{jsx,tsx}",
     }),
+    generateRoutes(),
   ],
 });
